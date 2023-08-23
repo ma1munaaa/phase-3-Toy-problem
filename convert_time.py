cfb56bdc2a8268ac840386b95abb09a3ce024f5a
maimuna003 <135684307+maimuna003@users.noreply.github.com>
2023-08-23 08:45:43 +0300

    Update convert_time.py

--- convert_time.py
+++ convert_time.py
@@ -4,19 +4,19 @@ def convert_to_24_hour(hour, minute, time_period):
             hour_24 = 0
         else:
             hour_24 = hour
-    else:  # time_period is "pm"
+    else:  
         if hour == 12:
             hour_24 = 12
         else:
             hour_24 = hour + 12
     
-    # Convert hour and minute to strings with leading zeros if necessary
+    
     hour_str = str(hour_24).zfill(2)
     minute_str = str(minute).zfill(2)
     
     return hour_str + minute_str
 
-# Test cases
+
 print(convert_to_24_hour(10, 30, "am"))  
 print(convert_to_24_hour(11, 20, "pm"))  
 print(convert_to_24_hour(6, 45, "am"))  
